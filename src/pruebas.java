83a40ca0be0df382552e2b10e651669ff8f86aa1
Ricardo_Torres <richarcom1306@gmail.com>
2022-03-20 17:27:49 -0600

    Aqui Comienza el primer commit con dos clases

---
+++ src/pruebas.java
@@ -0,0 +1,11 @@
+public class pruebas {
+
+    public static void main (String[] args){
+
+        int speed=10;
+        String bienvenida= "Hola mundo esto se va a poner al mil por hora + ";
+
+        System.out.println(bienvenida+speed);
+
+    }
+}
