a2c20e486de0bbca8a18eebe03e2db5abb6504e2
Ricardo_Torres <richarcom1306@gmail.com>
2022-03-22 21:29:42 -0600

    Version comentada con formato JavaDoc

--- src/FuncionesJava.java
+++ src/FuncionesJava.java
@@ -16,27 +16,53 @@ public class FuncionesJava {
         //(4/3)*pi * r3
         System.out.println(sphereVolum(y));
 
-        //Da el convertidor a Dll ingresando el peso y tipo de moneda.
+        //Da el convertidor a Dll ingresando el peso y tipo de moneda. MXN y POV
         System.out.println(convertToDollar(y,moneda));
 
     } //Fin del metodo main
 
+    /**
+     * Descripcion: Realiza el calculo del area de un circulo, recibiendo el radio.
+     *
+     * @param r recibe el radio del circulo a calcular.
+     * @return Arroja el area calculada del circulo.
+     * */
     //Las funciones se declaran a nivel e main en el mismo scoope.
     //Saca el area del circulo recibiendo solo el radio.
     public static double circleArea (double r){
         return  Math.PI * Math.pow(r,2);
     } //fin de la funcion.
 
+    /**
+     * Descripcion: Saca el area de una esfera recibiendo solamente el radio.
+     *
+     * @param r Recibe el radio como double.
+     * @return Regresa el area de la esfera en un valor double.
+     * */
     //Saca el area de una esfera
     public static double sphereArea (double r){
         return 4 * Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Descripcion: Arroja el volumen de una esfera solamente con recibir el radio.
+     *
+     * @param r Recibe el radio como double.
+     * @return devuelve el volumen en un valor double.
+     * */
     //Saca el volumen de una esfera, recibiendo un valor.
     public static double sphereVolum (double r){
         return (4/3) * Math.PI * Math.pow(r,3);
     }
 
+    /**
+     * Descripcion: Convierte moneda a dollar, solamente MXN y POV.
+     *
+     * @param m Recibe el valor de la moneda en double.
+     * @param t Recibe el String de MXN o POV
+     *
+     * @return Arroja el valor convertido en double.
+     * */
     //Cambia de valor monetario
     public static double convertToDollar (double m, String t){
         switch (t){
